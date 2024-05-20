5ac330fe9263ad6c740c8a9c8fd42b9303d99774
Official-Krish <krishanand0103@gmail.com>
2024-05-21 03:07:51 +0530

    Minor changes

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -24,6 +24,7 @@ export const Blog = () => {
         
   }
   return <div>
+      <Appbar />
       <FullBlog/>
   </div>
 }
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -33,7 +33,7 @@ export const Blogs = () => {
                     authorName={blog.author.name || "Anonymous"}
                     title={blog.title}
                     content={blog.content}
-                    publishedDate={"2nd Feb 2024"}
+                    publishedDate={"May 2024"}
                 />)}
             </div>
         </div>
