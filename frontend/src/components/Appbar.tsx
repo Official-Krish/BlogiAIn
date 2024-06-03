c2f27682208b77925810251d66e3453a163df7bc
Official-Krish <krishanand0103@gmail.com>
2024-06-04 00:22:43 +0530

    changed appbar

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,91 +1,69 @@
-import { useState } from "react";
-import { Avatar } from "./BlogCard";
-import { Link, useNavigate } from "react-router-dom";
-import WriteIcon from "./icons/WriteIcon";
-
+import { Link, useNavigate } from 'react-router-dom';
+import WriteIcon from './icons/WriteIcon';
+import ProfileBox from './ProfileBox';
+import Search from './Search';
 
 interface AppbarProps {
   skipAuthCheck?: boolean;
   pageActions?: JSX.Element;
   hideWriteAction?: boolean;
 }
-export const Appbar = ({ skipAuthCheck = false }: AppbarProps) => {
+
+export const Appbar = ({ skipAuthCheck = false, pageActions, hideWriteAction = false }: AppbarProps) => {
   const navigate = useNavigate();
-  const isUserLoggedIn = localStorage.getItem("token");
+  const isUserLoggedIn = localStorage.getItem('token');
+
   if (!isUserLoggedIn && skipAuthCheck == false) {
-    navigate("/signin");
+    navigate('/signin');
   }
   return (
     <div className="border-b border-slate-100 flex justify-between items-center p-4 md:px-16">
-      <Link to="/" className="text-xl font-bold">
-        Medium
-      </Link>
-      {isUserLoggedIn ? (
-        <div className="flex gap-4 md:gap-8">
-          <Link to="/publish">
-            <div
-            typeof="text"
-              className="focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium flex items-center gap-2 rounded-lg text-sm px-5 me-2 mb-2 mx-12 py-5"
-            >
-              <WriteIcon /> Write
-            </div>
-          </Link>
-          <ProfileBox />
-        </div>
-      ) : (
-        <Link
-          to="/signin"
-          className="focus:outline-none text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mx-12"
-        >
-          Sign In
+      <div className="flex justify-center items-center gap-4">
+        <Link to="/" className="text-xl font-light">
+        <span className="text-gray-700">figuring</span><span className="text-black">out</span><span className="text-green-700">Life</span>
         </Link>
-      )}
+        <Search />
+      </div>
+
+      <div className="flex items-center gap-1">
+        
+
+        {isUserLoggedIn ? (
+          <>
+            {hideWriteAction === false && (
+              <Link to="/publish">
+                <button
+                  type="button"
+                  className="focus:outline-none hover:bg-gray-100 rounded-3xl focus:ring-4 focus:ring-gray-100 font-medium flex items-center gap-2 text-sm px-5 py-2.5"
+                >
+                  <WriteIcon /> Write
+                </button>
+              </Link>
+            )}
+            {pageActions}
+            <div className="ml-4">
+              <ProfileBox />
+            </div>
+          </>
+        ) : (
+          <div className="ml-4">
+            <Link
+              to="/signup"
+              className="focus:outline-none text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5"
+            >
+              Sign Up
+            </Link>
+            <Link
+              to="/signin"
+              className="ml-4 focus:outline-none text-gray-700 border border-gray-700 hover:bg-gray-800 hover:text-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5"
+            >
+              Sign In
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
-
-function ProfileBox() {
-  const navigate = useNavigate();
-  const [show, setShow] = useState(false);
-  const nameAvatar = localStorage.getItem("emailForSignIn")
-
-  const logout = () => {
-    localStorage.removeItem("token");
-    navigate("/");
-  };
-  const goToBookmarks = () => {
-    navigate("/bookmarks");
-  };
-
-  const goToProfile = () => {
-    const userLocalStorage = localStorage.getItem("user");
-    if (userLocalStorage) {
-      const userInfo = JSON.parse(userLocalStorage);
-      navigate(`/${userInfo.id}`);
-    }
-  // navigate("/profile")
-  };
-
-
-  return (
-    <div className="relative cursor-pointer mt-2.5">
-      <Avatar name={nameAvatar} onClick={() => setShow(!show)} />
-      {show && (
-        <div className="absolute -bottom-24 -left-16 shadow-lg p-4 bg-gray-50 border border-gray-100 z-50 w-[160px]">
-          <div className="flex flex-col gap-3 px-4 py-2">
-            <div onClick={goToProfile}>Profile</div>
-            <div onClick={goToBookmarks}>Bookmarks</div>
-            <div onClick={logout}>Logout</div>
-          </div>
-          <div className="flex flex-col gap-3 px-4 py-2">
-            <div onClick={()=>{
-                        window.open("https://github.com/Official-Krish", '_blank', 'noopener,noreferrer');
-                    }}>Developer</div>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-}
 
 export default Appbar;
---
+++ frontend/src/components/Avatar.tsx
@@ -0,0 +1,48 @@
+type AvatarProps = {
+    name: string;
+    size?: 'small' | 'medium' | 'large';
+    imageSrc?: any;
+    onClick?: React.MouseEventHandler<HTMLDivElement>;
+  };
+  
+  export const Avatar = ({ name, onClick, size = 'medium', imageSrc }: AvatarProps) => {
+    const getDimensions = () => {
+      switch (size) {
+        case 'small':
+          return {
+            containerSize: 'w-5 h-5',
+          };
+        case 'medium':
+          return {
+            containerSize: 'w-10 h-10',
+          };
+        case 'large':
+          return {
+            containerSize: 'w-20 h-20',
+            textSize: 'text-xl',
+          };
+      }
+    };
+  
+    const { containerSize, textSize } = getDimensions();
+    return (
+      <div
+        onClick={onClick}
+        className={`cursor-pointer border-2 border-green-700 relative inline-flex items-center justify-center overflow-hidden bg-gray-800 hover:bg-gray-300 rounded-full ${containerSize}`}
+      >
+        {imageSrc ? (
+          <img src={imageSrc} />
+        ) : (
+          <>
+            <span className={`font-medium ${textSize} text-gray-200 `}>
+            {name?.split(' ')?.[0]?.[0] ?? ''}
+            {name?.split(' ')?.[1]?.[0] ?? ''}
+          </span>
+          </>
+        )}
+      </div>
+    );
+  };
+  
+  export default Avatar;
+  
