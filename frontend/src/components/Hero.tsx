bf90991ce54a7aee0d11e224fab4caddd7efc38f
Official-Krish <krishanand0103@gmail.com>
2024-07-03 13:40:39 +0530

    blogs bug fix

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,6 +1,7 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Hero = () => {
+	const navigate = useNavigate();
 	return (
 		<div className="w-[100] h-screen bg-gradient-to-r from-amber-200 to-yellow-500 flex flex-col justify-center items-center">
 			<h1 className="text-4xl md:text-7xl tracking-wide font-extrabold text-center md:text-left">
@@ -9,17 +10,23 @@ const Hero = () => {
 			<h6 className="text-2xl md:text-4xl tracking-wide font-light text-center md:text-left py-6">
 				Discover stories, thinking, and expertise from writers on any topic.
 			</h6>
-			<Link
-				to="/blogs"
-        className="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-gray-900 text-gray-800"
-			>
-					<span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-gray-900 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
+
+			<button onClick={ () => {
+				const token = localStorage.getItem("token");
+				if (token) {
+					navigate("/blogs");
+				} else {
+					navigate("/signin");
+				}
+			}} className="rounded-md px-3.5 py-2 m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-gray-900 text-gray-800">
+				<span className="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-gray-900 top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
 					<span className="relative text-gray-900 transition font-bold duration-300 group-hover:text-white ease text-lg tracking-wider" >
 						Start reading
 					</span>
-			</Link>
+			</button>
 		</div>
 	);
 };
 
 export default Hero;
+
