b7fbc864e74ea348e6896b0fade0dd9d21e37854
Official-Krish <krishanand0103@gmail.com>
2024-07-18 22:51:41 +0530

    updated homepage

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -56,7 +56,7 @@ export const Hero = () => {
                     navigate("/signin");
                   }
                 }}
-                className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-white mt-16"
+                className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-white mt-16 hover:bg-gray-300"
                
               >
                 Start Reading
