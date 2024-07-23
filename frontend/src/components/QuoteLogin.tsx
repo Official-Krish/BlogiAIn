c8183368057209b481930c310c2ce01833fd6854
Official-Krish <krishanand0103@gmail.com>
2024-07-23 22:43:26 +0530

    quote changed for login page

--- frontend/src/components/QuoteLogin.tsx
+++ frontend/src/components/QuoteLogin.tsx
@@ -3,20 +3,9 @@ export const QuoteLogin = () =>{
         <div className="flex justify-center">
             <div className="max-w-lg ">
                 <div className="text-3xl font-bold">
-                    "To laugh often and much: To win the respect of intelligent people
-                     and the affection of children, to earn the appreciation of honest
-                    critics and endure the betrayal of false friends; to appreciate
-                    beauty, to find the best in others, to leave the world a bit better
-                    whether by a healthy child, a garden patch, or a redeemed social
-                    condition; to know even one life has breathed easier because you
-                    lived. This is to have succeeded."
+                    Dive into a world where each blog post is a journey through new perspectives and innovative ideas, sparking conversations that matter.
                 </div>
-                <div className="max-w-md text-lg font-bold , mt-4">
-                  Ralph Waldo Emerson
-                </div>
-                <div className="max-w-md text-sm text-gray-500 font-medium">
-                artofpoets.com
-                </div>
+                
             </div>
         </div>
         
